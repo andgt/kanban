2fe5f6aaa3eb94df9fc801e25616332fbedadf60
anatoliy <anatolijdegtyar@yandex.ru>
2021-09-20 19:33:13 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@
 
 let taskForm = document.querySelector(".add-task__form");
 let mainList = document.querySelector(".taskboard__group--backlog .taskboard__list");
+let taskList = document.querySelector(".taskboard");
+let items = document.getElementsByClassName("taskboard__item");
 let newTaskField = document.getElementById("add-task");
 let taskTemplate = document.getElementById("task-template").content;
+let listTasks = document.querySelectorAll(".taskboard__list");
 
 // Добавляет новую задачу в список
 
@@ -17,13 +20,18 @@ taskForm.addEventListener("submit", (evt) => {
 	let task = newItemTask.cloneNode(true);
 	mainList.appendChild(task);
 	newTaskField.value = "";
+	listTasks.forEach(element => {
+		if (element.children.length > 1) {
+			let emptyThisSubmit = element.querySelector(".task--empty");
+			emptyThisSubmit.classList.add("task--invisible");
+		}
+	});
 });
 
 
 // Режим редактирования задачи вкл/выкл.
 
 let editTask = function (evt) {
-	let items = document.getElementsByClassName("taskboard__item");
 
 	document.addEventListener("click", (evt) => {
 		let taskInputs = document.querySelectorAll(".task__input");
@@ -62,4 +70,65 @@ let editTask = function (evt) {
 
 editTask();
 
+// Перемещение задач
 
+let draggedTask = function () {
+	listTasks.forEach(element => {
+		if (element.children.length < 2) {
+			let emptyThisVisible = element.querySelector(".task--empty");
+			emptyThisVisible.classList.remove("task--invisible");
+		} else if (element.children.length > 1) {
+			let emptyThisInvisible = element.querySelector(".task--empty");
+			emptyThisInvisible.classList.add("task--invisible");
+		}
+		element.addEventListener("mouseover", (evt) => {
+			for (let item of items) {
+				item.classList.contains("task--empty") ?
+				item.draggable = false :
+				item.draggable = true;
+			};
+		});
+
+		element.addEventListener("dragstart", (evt) => {
+			evt.target.classList.add("task--dragged");
+			if (element.children.length <= 2) {
+				let emptyThisInvisibleStart = element.querySelector(".task--empty");
+				console.log(emptyThisInvisibleStart);
+				emptyThisInvisibleStart.classList.remove("task--invisible");
+			}
+		});
+
+		element.addEventListener("dragend", (evt) => {
+			evt.target.classList.remove("task--dragged");
+			if (element.children.length > 1) {
+				let emptyThisVisibleEnd = element.querySelector(".task--empty");
+				emptyThisVisibleEnd.classList.add("task--invisible");
+			}
+		});
+
+		element.addEventListener("dragover", (evt) => {
+			evt.preventDefault();
+			let activeElement = taskList.querySelector(".task--dragged");
+			let currentElement = evt.target;
+			let isMoveable = activeElement != currentElement && currentElement.classList.contains("taskboard__item");
+			if (!isMoveable) {
+				return;
+			}
+			let nextElement = getNextElement(evt.clientY, currentElement);
+			if (nextElement && activeElement === nextElement.previousElementSibling || activeElement === nextElement) {
+				return;
+			}
+			element.insertBefore(activeElement, nextElement);
+		});
+	});
+	let getNextElement = (cursorPosition, currentElement) => {
+		let currentElementCoord = currentElement.getBoundingClientRect();
+		let currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+		let nextElement = (cursorPosition < currentElementCenter) ?
+		currentElement :
+		currentElement.nextSibling;
+		return nextElement;
+	};
+};
+
+draggedTask();
